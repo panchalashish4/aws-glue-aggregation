5e7e146dcdeafc01b58e3235c4a3926952587b69
ashishpanchal <apanchal8746@sdsu.edu>
2024-03-24 22:00:30 -0700

    added customer_id

--- upload_to_s3.py
+++ upload_to_s3.py
@@ -6,7 +6,7 @@ bucket_name = "customer-debit-card-purchase"
 
 def upload_to_s3(filename, date_str):
     """Uploads a CSV file to S3 with Hive-style partitioning"""
-    file_path = f"date={date_str}/{filename}"  # Hive-style partitioning
+    file_path = f"raw_data/date={date_str}/{filename}"  # Hive-style partitioning
     s3.upload_file(f'/tmp/{filename}', bucket_name, file_path)
     print(f"File uploaded to S3 Bucket {bucket_name}/{file_path}")
     return
